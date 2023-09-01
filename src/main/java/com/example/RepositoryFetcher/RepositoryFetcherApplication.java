fea40d13751a24cd56e674d29f50e33010ef4332
Przemek Rylski <przemekrylski@gmail.com>
2023-09-01 15:23:06 +0200

    Initial commit

---
+++ src/main/java/com/example/RepositoryFetcher/RepositoryFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.RepositoryFetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RepositoryFetcher/RepositoryFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RepositoryFetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
